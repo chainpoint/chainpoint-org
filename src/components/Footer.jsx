17655ec4a029fd0e865fc9b3f0b4bc240be60e56
Cody Henshaw <cody@tierion.com>
2018-07-26 14:48:12 -0700

    Updated footer urls to point to chainpoint

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,7 +10,7 @@ const Footer = () => {
             <p className='left'><span className='copyright'>&copy; {year}</span></p>
           </div>
           <div className='d-none d-sm-block'>
-            <p className='right'><span className='social'><a href='https://www.facebook.com/pages/Tierion/1430447893927429' target='_blank' className='facebook' /><a href='http://www.twitter.com/tierion' target='_blank' className='twitter' /></span></p>
+            <p className='right'><span className='social'><a href='https://www.facebook.com/chainpoint' target='_blank' className='facebook' /><a href='https://twitter.com/chainpnt' target='_blank' className='twitter' /></span></p>
           </div>
           <p className='small-date d-block d-sm-none'>&copy; {year}</p>
           <a id='flogo' href='#' className='go-top' /><a href='https://tierion.com' target='_blank' className='site-by d-none d-sm-block'>site by Tierion</a>

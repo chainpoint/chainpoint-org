259a608286d30fecba2a361fd8253ac49659c695
Jason Bukowski <jbukowski@gmail.com>
2020-04-09 08:58:38 -0400

    Added 404 page

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,8 +1,22 @@
 <template>
-  <div class="error-page">
-    <h1 v-if="error.statusCode === 404">NOT FOUND</h1>
-    <h1 v-else>An error occurred</h1>
-  </div>
+  <section class="section section-404">
+    <div class="container">
+      <div class="row justify-content-md-center">
+        <div v-if="error.statusCode === 404" class="section-title col col-md-8">
+          <h1>Not Found</h1>
+          <h2>
+            The page you are looking for doesn't exist
+          </h2>
+        </div>
+        <div v-else class="section-title col col-md-8">
+          <h1>Something went wrong :(</h1>
+          <h2>
+            An unexpected error occurred, try again
+          </h2>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
@@ -12,7 +26,6 @@ export default {
       type: Object,
       default: () => {}
     }
-  },
-  layout: 'default-error'
+  }
 }
 </script>

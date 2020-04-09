2e9840a4fcf7b142f93bd99b87d2a82d4faba6bf
Jason Bukowski <jbukowski@gmail.com>
2020-04-09 09:51:05 -0400

    Make nav menu togglable

--- components/PageHeader.vue
+++ components/PageHeader.vue
@@ -9,14 +9,15 @@
         <div class="nav-menu" :class="{ 'is-visible': mobileNavOpen }">
           <ul class="navbar-nav bd-navbar-nav flex-row">
             <li v-for="navLink in navLinks" :key="navLink.title" class="nav-item">
-              <nuxt-link class="nav-link" :to="navLink.path"
-                ><span>{{ navLink.title }}</span>
+              <nuxt-link class="nav-link" :to="navLink.path" @click.native="toggleMobileNav">
+                <span>{{ navLink.title }}</span>
               </nuxt-link>
             </li>
           </ul>
         </div>
-        <button class="nav-burger">
-          <NavBurger />
+        <button class="nav-burger" @click="toggleMobileNav">
+          <NavBurger v-if="!mobileNavOpen" />
+          <NavClose v-else />
         </button>
       </div>
     </div>
@@ -26,15 +27,17 @@
 <script>
 import TierionLogo from '@/assets/images/header/tierion-logo.svg?inline'
 import NavBurger from '@/assets/images/header/nav-burger.svg?inline'
+import NavClose from '@/assets/images/header/nav-close.svg?inline'
 
 export default {
   components: {
     TierionLogo,
-    NavBurger
+    NavBurger,
+    NavClose
   },
   data() {
     return {
-      mobileNavOpen: true,
+      mobileNavOpen: false,
       navLinks: [
         {
           title: 'Link 1',
@@ -45,6 +48,11 @@ export default {
           path: '/link2'
         }
       ]
+    }
+  },
+  methods: {
+    toggleMobileNav() {
+      this.mobileNavOpen = !this.mobileNavOpen
     }
   }
 }

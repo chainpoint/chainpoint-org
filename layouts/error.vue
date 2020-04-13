fc28c97804ba3c736f1665a784ab7aef6352d68f
Jason Bukowski <jbukowski@gmail.com>
2020-04-13 11:21:51 -0400

    Add 404 page

---
+++ layouts/error.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="error-page">
+    <h1>This page does not exist</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: 'default-error'
+}
+</script>
--- nuxt.config.js
+++ nuxt.config.js
@@ -76,5 +76,8 @@ export default {
      ** You can extend webpack config here
      */
     extend(config, ctx) {}
+  },
+  generate: {
+    fallback: true
   }
 }
